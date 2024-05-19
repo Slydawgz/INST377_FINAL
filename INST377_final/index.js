3a43fbdce54c0cf743b08a7894ee16242189baeb
Slydawgz <47046452+Slydawgz@users.noreply.github.com>
2024-05-19 19:06:43 -0400

    Update index.js

--- INST377_final/index.js
+++ INST377_final/index.js
@@ -10,6 +10,7 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', async (req, res) => {
   try {
